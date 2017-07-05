061787c266cafa2185b4c2123f3627ef4ca92f51
Sreeragh <sreeragh@qburst.com>
2017-07-05 10:56:00 +0530

    loss function, desired output, assign variable

--- started.py
+++ started.py
@@ -3,13 +3,22 @@ sess = tf.Session()
 
 
 W = tf.Variable([3], dtype=tf.float32)
-b = tf.Variable([1.0],dtype=tf.float32)
+b = tf.Variable([-3],dtype=tf.float32)
 x = tf.placeholder(tf.float32)
-
+# y is the desired output
+y = tf.placeholder(tf.float32)
 linear_model = W * x + b
-
+squared_deltas = tf.square(linear_model - y)
+#loss functin
+loss = tf.reduce_sum(squared_deltas)
+#initialize variables
 init = tf.global_variables_initializer()
 sess.run(init)
 
-print(sess.run(linear_model, {x:[1,2,3]}) )
+#assigning value to variable 
+fixW = tf.assign(W,[-1])
+fixb = tf.assign(b, [1])
+sess.run([fixW, fixb])
 
+print(sess.run(linear_model, {x:[1,2,3], y:[4, 7, 10]}))
+print sess.run(loss,{x:[1,2,3], y:[0, -1, -2]})
