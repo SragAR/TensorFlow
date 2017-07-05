428e56bef1a4fb62deb5695c5402455a216f9d17
Sreeragh <sreeragh@qburst.com>
2017-07-05 10:00:17 +0530

    variables

--- started.py
+++ started.py
@@ -1,22 +1,15 @@
 import tensorflow as tf
-
-node1 = tf.constant(3.0)
-node2 = tf.constant(4.0, dtype=tf.float32)
-node3 = tf.add(node1, node2)
-
 sess = tf.Session()
-print node1, node2, node3
-print(sess.run([node1,node2]))
-print(sess.run(node3))
 
-#parameterized
 
-a =tf.placeholder(tf.float32)
-b= tf.placeholder(tf.float32)
-adder_node = a + b #shorthand for tf.add(a,b)
-add_and_triple = adder_node * 3
+W = tf.Variable([3], dtype=tf.float32)
+b = tf.Variable([1.0],dtype=tf.float32)
+x = tf.placeholder(tf.float32)
 
-print(sess.run(adder_node, {a:5, b:3.5}))
-print(adder_node, {a:[1,2], b:[5,6]})
-print(sess.run(add_and_triple, {a:2, b:6}))
+linear_model = W * x + b
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+print(sess.run(linear_model, {x:[1,2,3]}) )
 
